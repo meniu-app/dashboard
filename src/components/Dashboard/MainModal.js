16525fa7155d6e6bce22b0a710cd8ecb10708efe
Victor Hernandez <vik407@gmail.com>
2021-03-20 23:30:50 -0500

    Main Modal

--- src/components/Dashboard/MainModal.js
+++ src/components/Dashboard/MainModal.js
@@ -19,8 +19,10 @@ class MainModal extends Component {
                                 <div className="col-12 d-flex py-2">
                                     <img src="https://via.placeholder.com/80C/O" alt=""/>
                                     <div className="mx-3">
-                                        <h4>Item</h4>
-                                        <button className="btn btn-primary" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#itemModal">Add an item to any category</button>
+                                        <button className="btn-sel btn btn-lg" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#itemModal">
+                                            <span className="badge bg-light"><i className="fas fa-2x fa-hamburger"></i></span>
+                                            Add an item to any category
+                                        </button>
                                     </div>
                                 </div>
                                 }
@@ -28,8 +30,10 @@ class MainModal extends Component {
                                 <div className="col-12 d-flex py-2">
                                     <img src="https://via.placeholder.com/80C/O" alt=""/>
                                     <div className="mx-3">
-                                        <h4>Category</h4>
-                                        <button className="btn btn-primary" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#categoryModal">Add a category to your menu</button>
+                                        <button className="btn-sel btn btn-lg" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#categoryModal">
+                                            <span className="badge bg-light"><i className="fas fa-2x fa-tasks"></i></span>
+                                            Add a category to your menu
+                                        </button>
                                     </div>
                                 </div>
                                 }
@@ -37,26 +41,33 @@ class MainModal extends Component {
                                 <div className="col-12 d-flex py-2">
                                     <img src="https://via.placeholder.com/80C/O" alt=""/>
                                     <div className="mx-3">
-                                        <h4>Menu</h4>
-                                        <button className="btn btn-primary" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#menuModal">Add a menu to your store</button>
+                                        <button className="btn-sel btn btn-lg" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#menuModal">
+                                            <span className="badge bg-light"><i className="far fa-2x fa-file-alt"></i></span>
+                                            Add a menu to your store
+                                        </button>
                                     </div>
                                 </div>
                                 }
                                 { (getUserRole() === 'admin' || (getUserRole() === 'owner' && restaurantDataReady)) &&
                                 <div className="col-12 d-flex py-2">
-                                    <img src="https://via.placeholder.com/80C/O" alt=""/>
+                                    <div className="icon-modal">
+                                        
+                                    </div>
                                     <div className="mx-3">
-                                        <h4>New store user</h4>
-                                        <button className="btn btn-primary" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#userModal">Add new store user</button>
+                                        <button className="btn-sel btn btn-lg" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#userModal">
+                                            <span className="badge bg-light"><i className="fas fa-2x fa-user-plus"></i></span>
+                                            Add new store user
+                                        </button>
                                     </div>
                                 </div>
                                 }
                                 { (getUserRole() === 'admin' || (getUserRole() === 'owner' && !restaurantDataReady)) && 
                                 <div className="col-12 d-flex py-2">
-                                    <img src="https://via.placeholder.com/80C/O" alt=""/>
                                     <div className="mx-3">
-                                        <h4>Restaurant</h4>
-                                        <button className="btn btn-primary" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#restaurantModal">Add a new restaurant</button>
+                                        <button className="btn-sel btn btn-lg" data-bs-dismiss="modal" aria-label="Close" data-bs-toggle="modal" data-bs-target="#restaurantModal">
+                                            <span className="badge bg-light"><i className="fas fa-2x fa-utensils"></i></span>
+                                            Add a new restaurant
+                                        </button>
                                     </div>
                                 </div>
                                 }
