69fa9b27ecd09b431f9c7fe01055ca9736c4906f
Victor Hernandez <vik407@gmail.com>
2021-03-23 07:27:25 -0500

    profile fix

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,29 +1,45 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { getUser } from '../../api/TokenHandler';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { getUser } from "../../api/TokenHandler";
 
 class Profile extends Component {
   render() {
     const user = getUser();
-  
+
     return (
       <div className="container">
-          <h3 className="my-3">Settings</h3>
-          <div className="ms-4">
-            <div className="d-flex mb-3">
-              <h5>Account info</h5>
-              <Link to="/settings/edit" className="ms-5">Edit</Link>
+        <div className="row">
+          <div className="col">
+            <h3 className="my-3">Settings</h3>
+            <div className="ms-4">
+              <div className="d-flex mb-3">
+                <h5>Account info</h5>
+                <Link to="/settings/edit" className="ms-5">
+                  Edit
+                </Link>
+              </div>
+              <h6>
+                <b>Name</b>
+              </h6>
+              <p>
+                {user.first_name} {user.last_name}
+              </p>
+              <h6>
+                <b>Address</b>
+              </h6>
+              <p>100 Main Stree</p>
+              <div className="d-flex">
+                <h6>Password</h6>
+                <Link to="/settings/change_password" className="ms-5">
+                  Edit
+                </Link>
+              </div>
+              <h6>
+                <b>*************</b>
+              </h6>
             </div>
-            <h6><b>Name</b></h6>
-            <p>{user.first_name} {user.last_name}</p>
-            <h6><b>Address</b></h6>
-            <p>100 Main Stree</p>
-            <div className="d-flex">
-              <h5>Password</h5>
-              <Link to="/settings/change_password" className="ms-5">Edit</Link>
-            </div>
-            <h6><b>*************</b></h6>
           </div>
+        </div>
       </div>
     );
   }
